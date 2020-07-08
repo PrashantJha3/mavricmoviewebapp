63252d6f43f0fbbd12ec86f1125699aa89382a2e
Prashant Jha <jha.prashant654@gmail.com>
2020-07-08 15:18:24 +0530

    new back link has been added into navigation page header

--- src/components/elements/Navigation/Navigation.js
+++ src/components/elements/Navigation/Navigation.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
+import Movie from '../../Movie/Movie';
 import './Navigation.css';
 
 const Navigation = ({ movie }) => (
   <div className="rmdb-navigation">
     <div className="rmdb-navigation-content">
+	
       <Link to="/">
         <p>Home</p>
       </Link>
       <p>/</p>
+	  <BrowserRouter>
+	<Switch>
+	 <Route path="/:movieId" component={Movie} exact />
+	  <p>Back</p>
+	  </Switch>
+	  </BrowserRouter>
+	  <p>/</p>
+	
       <p>{movie}</p>
+	  
     </div>
   </div>
 )
